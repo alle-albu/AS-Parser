21c606019aa0ed325649cb42c4d55041f54a8ee4
leooonard8 <leonard.filip.93@gmail.com>
2018-10-19 17:26:38 +0300

    Update a.c Am acoperit branchul de ERROR sat nu am putut compila in avion :).

--- a.c
+++ a.c
@@ -36,8 +36,12 @@ STATE_MACHINE_RETURN_VALUE at_command_parse(uint8_t current_character){
     }
     case 2:  {
         if (current_character == 'O') {
+          //follow OK branch
             state = 3;
-        }  else  {
+        } else  if (current_character == 'E') {
+          //follow ERROR branch
+          state = 6;
+            } else {
             // RESET AUTOMATON
             state = 0;
             data.ok = 0;
@@ -64,7 +68,7 @@ STATE_MACHINE_RETURN_VALUE at_command_parse(uint8_t current_character){
     }
     case 4:  {
         if (current_character == 0x0D) {
-            state = 11;
+            state = 5;
         }  else  {
             // RESET AUTOMATON
             state = 0;
@@ -89,6 +93,79 @@ STATE_MACHINE_RETURN_VALUE at_command_parse(uint8_t current_character){
             return STATE_MACHINE_READY_WITH_ERROR;
         }
     }
+    
+    case 6: {
+        if (current_character == 'E') {
+            state = 7;
+        } else {
+            // RESET AUTOMATON
+            state = 0;
+            data.ok = 0;
+            data.line_count = 0;
+            memset(data.data, 0, sizeof data.data);
+
+            return STATE_MACHINE_READY_WITH_ERROR;
+        }
+    }
+    
+    case 7: {
+        if (current_character == 'R') {
+            state = 8;
+        } else {
+            // RESET AUTOMATON
+            state = 0;
+            data.ok = 0;
+            data.line_count = 0;
+            memset(data.data, 0, sizeof data.data);
+
+            return STATE_MACHINE_READY_WITH_ERROR;
+        }
+    }
+    
+    case 8: {
+        if (current_character == 'R') {
+            state = 9;
+        } else {
+            // RESET AUTOMATON
+            state = 0;
+            data.ok = 0;
+            data.line_count = 0;
+            memset(data.data, 0, sizeof data.data);
+
+            return STATE_MACHINE_READY_WITH_ERROR;
+        }
+    }
+    
+    case 9: {
+        if (current_character == 'O') {
+            state = 10;
+        } else {
+            // RESET AUTOMATON
+            state = 0;
+            data.ok = 0;
+            data.line_count = 0;
+            memset(data.data, 0, sizeof data.data);
+
+            return STATE_MACHINE_READY_WITH_ERROR;
+        }
+    }
+/*
+although "<CR><LF>ERROR" should lead to a separate unique state, we can reuse state 4 since a "<CR><LF>" is expected on this ERROR branch
+*/  
+    case 10: {
+        if (current_character == 'R') {
+            state = 4;
+        } else {
+            // RESET AUTOMATON
+            state = 0;
+            data.ok = 0;
+            data.line_count = 0;
+            memset(data.data, 0, sizeof data.data);
+
+            return STATE_MACHINE_READY_WITH_ERROR;
+        }
+    }
+    
     case 11:  {
             data.ok = 1;
             print_data(data);
