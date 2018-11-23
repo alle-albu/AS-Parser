e7c04d8833f82b292be6dd5c759c05ea5a4bb119
leooonard8 <leonard.filip.93@gmail.com>
2018-11-23 13:15:34 +0200

    Minor changes and some ideas for unimplemented functions

--- b.c
+++ b.c
@@ -15,6 +15,7 @@ AT_COMMAND_DATA data_structure;
 const uint8_t*  at_command_simple="AT\r\n";
 const uint8_t*  at_command_csq = "AT+CSQ\r\n";
 timer_software_handler_t my_timer_handler;
+timer_software_handler_t response_timer_handler;
 
 void ExecuteCommand(const uint8_t *command)
 {
@@ -38,9 +39,9 @@ void GetCommandResponse()
   // parse and extract data
   uint8_t ch;
   bool ready = false;
-  TIMER_SOFTWARE_reset_timer(my_timer_handler);
-  TIMER_SOFTWARE_start_timer(my_timer_handler);
-  while ((!TIMER_SOFTWARE_interrupt_pending(my_timer_handler)) && (ready == false))
+  TIMER_SOFTWARE_reset_timer(response_timer_handler);
+  TIMER_SOFTWARE_start_timer(response_timer_handler);
+  while ((!TIMER_SOFTWARE_interrupt_pending(response_timer_handler)) && (ready == false))
   {
     while (DRV_UART_BytesAvailable(UART_3) > 0)
     {
@@ -59,10 +60,13 @@ uint32_t ConvertAsuToDbmw(uint32_t rssi_value_asu){
 
 uint32_t ExtractAsu(AT_COMMAND_DATA *data_structure){
   //  @TODO: implement this
+
+	//idea: add an data_structure parameter to the parser function, and define it globally here in order to have it available for the function 
 }
 
 bool CommandResponseValid(){
   // @TODO: implement this
+	//idea: define a global variable of type STATE_MACHINE_RETURN_VALUE, instantiate it in GetCommandResponse() and return true here only if it is STATE_MACHINE_READY_OK, STATE_MACHINE_READY_WITH_ERROR
 }
 
 void BoardInit()
@@ -91,16 +95,20 @@ int main(void)
 
 // init, configure and start timer
   TIMER_SOFTWARE_init_system(); 
-  my_timer_handler = TIMER_SOFTWARE_request_timer(); 
-  if (my_timer_handler < 0) 
+  my_timer_handler = TIMER_SOFTWARE_request_timer();
+  response_timer_handler = TIMER_SOFTWARE_request_timer();
+  if (my_timer_handler < 0 || response_timer_handler < 0) 
   {
-    printf("Timer could not be init.");
+    printf("Timers could not be init.");
   } 
-  TIMER_SOFTWARE_configure_timer(my_timer_handler, MODE_1, 100, true);
+  //a request to the GSM modem should be sent every 1 sec (therefore timer is configured with 1000ms)
+  TIMER_SOFTWARE_configure_timer(my_timer_handler, MODE_1, 1000, true);
+  //internal timer configuration, if GSM Modem gets blocked after the request, execution will still continue after 500ms
+  TIMER_SOFTWARE_configure_timer(response_timer_handler, MODE_1, 500, true);
   TIMER_SOFTWARE_start_timer(my_timer_handler); 
 
 
-// establish connection to UART3 by sending AT command 3 times
+// establish connection to UART3 by sending AT command 3 times / configure the GSM modem AUTOBAUD by sending the AUTOBAUD pattern 3 times 
   for(int i=0;i<3;i++){
     char * my_at_command_simple = strdup(at_command_simple);
     DRV_UART_Write(UART_3, my_at_command_simple, strlen(my_at_command_simple));
